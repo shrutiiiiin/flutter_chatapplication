5817a36466e681a8429f40e47d69efdcaa06e139
shrutiiiiin <shrutinaik2317@gmail.com>
2024-05-26 01:56:14 +0530

    update

--- lib/screens/WelcomeScreen.dart
+++ lib/screens/WelcomeScreen.dart
@@ -1,4 +1,3 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:chatapplication/screens/registrationScreen.dart';
 import 'package:chatapplication/screens/loginScreen.dart';
 import 'package:flutter/material.dart';
